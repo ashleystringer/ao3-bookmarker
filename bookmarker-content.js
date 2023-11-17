2b698d40a92a6a07d3842b73222e49db351bf86f
ashleystringer <ashleylstringer@gmail.com>
2023-11-17 03:33:29 -0500

    Highlighting functional

--- bookmarker-content.js
+++ bookmarker-content.js
@@ -8,97 +8,33 @@ const bookmarker = () => {
 
   const parentNode = anchorNode.parentNode.innerText;
 
+  console.log(selectionObject);
   console.log(parentNode);
   console.log(parentNode.length);
-  /*
-    - Do the anchorNode and focusNode have the same parent element?
-    - Do the anchorNode and focusNode have the same sibling element?
-  */
+  console.log(anchorNode.parentNode);
+  console.log(focusNode.parentNode);
+
 
   if (anchorNode.parentNode === focusNode.parentNode) {
-    //do something
-    //anchorNode?.nextSibling === focusNode?.nextSibling
-    const parentNode = anchorNode.parentNode;
-    if (parentNode.childNodes.length == 1) {
-      console.log("Single element");
-      //place the smallest number in the first index
-      //place the largest number in the second index
-      insertBookmarkSpan(selectionObject, parentNode);
-    } else {
-      const parentNode = anchorNode.parentNode;
-      console.log(parentNode.childNodes);
+    
+    insertBookmarkSpan(selectionObject, anchorNode.parentNode);
 
-      if (
-        anchorNode.previousSibling == focusNode.previousSibling &&
-        anchorNode.nextSibling == focusNode.nextSibling
-      ) {
-        console.log("testing");
-        insertBookmarkSpan(selectionObject, parentNode);
-      } else {
-        findSelectionLocation(selectionObject);
-      }
-    }
   }
 };
 
 const insertBookmarkSpan = (selectionObject, parentNode) => {
   const parentHTML = parentNode.innerHTML;
+  console.log(parentHTML);
 
-  const spanElement = `<span class='bookmarker'>${parentHTML.slice(
-    selectionObject.anchorOffset,
-    selectionObject.focusOffset
-  )}</span>`;
-  const beforeSpan = parentHTML.slice(0, selectionObject.anchorOffset);
-  const afterSpan = parentHTML.slice(
-    selectionObject.focusOffset,
-    parentHTML.length
-  );
+  const range = document.createRange();
+  range.setStart(selectionObject.anchorNode, selectionObject.anchorOffset);
+  range.setEnd(selectionObject.focusNode, selectionObject.focusOffset);
 
-  console.log(beforeSpan);
-  console.log(spanElement);
-  console.log(afterSpan);
+  const spanElement = document.createElement("span");
+  spanElement.className = "bookmarker";
 
-  beforeSpan.concat(spanElement);
-  const alteredHTML = beforeSpan.concat(spanElement, afterSpan);
-  parentNode.innerHTML = alteredHTML;
-};
-
-const findSelectionLocation = selectionObject => {
-  const range = selectionObject.getRangeAt(0);
-  console.log(range);
-  const rects = range.getClientRects();
-  console.log(rects);
-  /*
-  - find the total length of each child node's inner text
-          - determine which child nodes are affected by the selection object's location
-          - find the absolute position of the selection object
-        */
-
-  const parentNode = selectionObject.anchorNode.parentNode;
-  console.log(selectionObject);
-  console.log(parentNode);
-  const childNodeLengthsObj = {};
-  parentNode.childNodes.forEach((child, index) => {
-    if (child.nodeName !== "BR" && child.nodeName !== "#text") {
-      console.log(child.nodeName);
-      childNodeLengthsObj[child.className] = child.innerText.length;
-    } else if (child.nodeName !== "BR" && child.nodeName == "#text") {
-      childNodeLengthsObj[`TEXT-${index}`] = child.length;
-    } else {
-      childNodeLengthsObj[child.className] = 0;
-    }
-  });
-  console.log(Object.entries(childNodeLengthsObj));
-
-  const anchorNode = selectionObject.anchorNode;
-  const focusNode = selectionObject.focusNode;
-
-  if (anchorNode.nextSibling == focusNode.previousSibling) {
-    console.log("anchorNode.nextSibling == focusNode.previousSibling");
-  } else {
-    console.log("anchorNode.nextSibling != focusNode.previousSibling");
-  }
-  //return [];
+  spanElement.appendChild(range.extractContents());
+  range.insertNode(spanElement);
 };
 
 const addIds = () => {
